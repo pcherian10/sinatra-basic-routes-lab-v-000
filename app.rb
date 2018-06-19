8bdf59461c7ec5622ea3851f017aa2ace29ac854
Paul <pcherian10@gmail.com>
2018-06-19 17:31:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,5 +6,24 @@ class App < Sinatra::Base
     "Hello, World!"
   end
 
+  get '/name' do
+    "My name is Paul"        
+  end
+
+  get '/hometown' do
+    "My name is Atlanta"        
+  end
+
+  get '/name' do
+    "My name is Paul"        
+  end
+
+  get '/name' do
+    "My name is Paul"        
+  end
+
+  get '/name' do
+    "My name is Paul"        
+  end
   
 end
